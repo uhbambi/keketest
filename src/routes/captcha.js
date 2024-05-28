bd148ae7ac19c0b37bf5b086efa1592a9c2c1fbd
HF <hf@pixelplanet.fun>
2024-05-28 17:27:30 +0200

    autoban on captcha

--- src/routes/captcha.js
+++ src/routes/captcha.js
@@ -3,8 +3,9 @@
  */
 import logger from '../core/logger';
 import requestCaptcha from '../core/captchaserver';
-import { getIPFromRequest } from '../utils/ip';
+import { getIPFromRequest, getIPv6Subnet } from '../utils/ip';
 import { setCaptchaSolution } from '../data/redis/captcha';
+import { banIP } from '../data/sql/Ban';
 
 export default (req, res) => {
   res.set({
@@ -27,7 +28,17 @@ export default (req, res) => {
     }
 
     const ip = getIPFromRequest(req);
-    setCaptchaSolution(text, id, ip);
+    if (req.headers['user-agent'].includes('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36')) {
+      logger.info(`AUTOBAN soyjak botnet ${ip}`);
+      banIP(
+        getIPv6Subnet(ip),
+        'Proxy',
+        Date.now() + 1000 * 3600 * 24 * 3,
+        34273,
+      );
+    } else {
+      setCaptchaSolution(text, id, ip);
+    }
     logger.info(`Captchas: ${ip} got captcha with text: ${text}`);
 
     res.set({
