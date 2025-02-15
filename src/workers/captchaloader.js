fcf61a28e70af050587f69fb16c0a5b3d1b3a20f
HF <hf@pixelplanet.fun>
2025-02-15 18:50:33 +0100

    update ppfun-captchas

--- src/workers/captchaloader.js
+++ src/workers/captchaloader.js
@@ -10,6 +10,7 @@ import ppfunCaptcha from 'ppfun-captcha';
 import { isMainThread, parentPort } from 'worker_threads';
 
 import { getRandomString } from '../core/utils';
+import filter from '../funcs/captchaFilter';
 
 const FONT_FOLDER = path.resolve(__dirname, '..', 'captchaFonts');
 
@@ -51,6 +52,7 @@ function createCaptcha() {
     style: 'stroke-width: 4;',
     background: '#EFEFEF',
     font,
+    filter,
   });
 }
 
