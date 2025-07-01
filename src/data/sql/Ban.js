f474809ca3183b170ca0cfa69a7e2ec642be418c
HF <hf@pixelplanet.fun>
2025-07-02 01:01:26 +0200

    fix bans not being displayed

--- src/data/sql/Ban.js
+++ src/data/sql/Ban.js
@@ -425,8 +425,8 @@ export async function getBanInfos(
       `SELECT b.*, md.name as mname, BIN_TO_UUID(b.uuid) AS buuid, BIN_TO_IP(ib.ip) AS 'ips.ipString' FROM Bans b
   LEFT JOIN Users md ON md.id = b.muid
   LEFT JOIN IPBans ib ON ib.bid = b.id
-WHERE (b.flags & ?) = ? AND (b.expires > NOW() OR b.expires IS NULL) AND b.id ${bidFilterMask}`, {
-        replacements: [flagMask, flagMask].concat(replacements),
+WHERE (b.flags & ?) > 0 AND (b.expires > NOW() OR b.expires IS NULL) AND b.id ${bidFilterMask}`, {
+        replacements: [flagMask].concat(replacements),
         raw: true,
         type: QueryTypes.SELECT,
       }));
