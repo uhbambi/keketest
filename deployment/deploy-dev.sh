7e882b70b2fc889a32f4a4de9b0731888402cba7
HF <hf@pixelplanet.fun>
2025-07-01 12:17:22 +0200

    move banlists into a nginx config creation script

--- deployment/deploy-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-tar --exclude='./dist/tiles' --exclude='./dist/log' -cvJf /tmp/ppfun.tar.xz ./dist
-scp /tmp/ppfun.tar.xz pixelplanet:/tmp/
-rm /tmp/ppfun.tar.xz
-ssh pixelplanet ./dev-deploy-from-temp.sh
