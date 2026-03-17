6a2e4f0e29bdb555b4390287952fa522865e8979
HF <hf@pixelplanet.fun>
2026-03-17 23:28:48 +0100

    add patch function for states

---
+++ src/store/index.js
@@ -0,0 +1,160 @@
+/*
+ * state related functions
+ */
+
+/**
+ * Apply a patch to a state. In example:
+ * Add a channel:
+ * {
+ *   op: 'add',
+ *   path: 'channels.1'
+ *   value: [cid, name, lastTs, lastReadTs, muted, avatarId]
+ * }
+ * Change channel Avatar:
+ * {
+ *   op: 'set',
+ *   path: 'channels.1[0:cid][5]',
+ *   value: avatarId,
+ * }
+ * Remove faction with the id: 1234:
+ * {
+ *   op: 'del',
+ *   path: 'factions[id:1234]',
+ * }
+ * @param state
+ * @param patch { op, path, [value] }
+ * @return [newState, firstTarget, success]
+ */
+export function patchState(state, patch) {
+  const { path } = patch;
+  const newState = { ...state };
+
+  let failed = false;
+  let firstTarget = null;
+
+  let location = newState;
+  let locationIndex = null;
+  let sectionStart = 0;
+
+  let i = 0;
+  while (i < path.length) {
+    const char = path[i];
+    if (char === '.' || char === '[') {
+      let locationTarget;
+      if (locationIndex || locationIndex === 0) {
+        if (sectionStart !== i) {
+          failed = true;
+          break;
+        }
+        locationTarget = locationIndex;
+        locationIndex = null;
+      } else {
+        locationTarget = path.substring(sectionStart, i);
+        if (!firstTarget) {
+          firstTarget = locationTarget;
+        }
+      }
+      const newLocation = location[locationTarget];
+      if (!newLocation || typeof newLocation !== 'object') {
+        failed = true;
+        break;
+      }
+      if (char === '.') {
+        /* object in tree */
+        if (Array.isArray(newLocation)) {
+          failed = true;
+          break;
+        }
+        location[locationTarget] = { ...newLocation };
+        location = location[locationTarget];
+        sectionStart = i + 1;
+      } else {
+        /* array in tree */
+        if (!Array.isArray(newLocation)) {
+          failed = true;
+          break;
+        }
+        location[locationTarget] = [...newLocation];
+        location = location[locationTarget];
+        i += 1;
+        /* array index */
+        const endIndex = path.indexOf(']', i);
+        if (endIndex === -1) {
+          failed = true;
+          break;
+        }
+        let index = path.substring(i, endIndex);
+        if (index.includes(':')) {
+          const [key, value] = index.split(':');
+          // eslint-disable-next-line eqeqeq
+          index = location.findIndex((l) => l?.[key] == value);
+          if (index === -1) {
+            failed = true;
+            break;
+          }
+          locationIndex = index;
+        } else {
+          locationIndex = Number(index);
+          if (Number.isNaN(locationIndex)) {
+            failed = true;
+            break;
+          }
+        }
+        i = endIndex;
+        sectionStart = i + 1;
+      }
+    }
+    i += 1;
+  }
+
+  let target;
+  if (!failed) {
+    if (locationIndex || locationIndex === 0) {
+      if (sectionStart !== path.length) {
+        failed = true;
+      } else {
+        target = locationIndex;
+      }
+    } else {
+      target = path.substring(sectionStart);
+      if (!firstTarget) {
+        firstTarget = target;
+      }
+    }
+  }
+
+  const { op, value } = patch;
+  if (!failed) {
+    switch (op) {
+      case 'add': {
+        if (!location[target]) {
+          location[target] = [value];
+        } else if (!Array.isArray(location[target])) {
+          failed = true;
+        } else {
+          location[target].push(value);
+        }
+        break;
+      }
+      case 'set': {
+        location[target] = value;
+        break;
+      }
+      case 'del': {
+        if (!location[target]) {
+          failed = true;
+        } else {
+          delete location[target];
+        }
+        break;
+      }
+      default:
+        failed = true;
+    }
+  }
+
+  if (failed || !patch.op) {
+    return [state, firstTarget, false];
+  }
+  return [newState, firstTarget, true];
+}
